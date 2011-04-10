28eb3068b2d95a8b2e8856dcb0f7ec022a850ace
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2011-04-10 13:57:42 -0400

    method to send albums to musicbrainz collection

--- __init__.py
+++ __init__.py
@@ -1,5 +1,18 @@
 from beets.plugins import BeetsPlugin
 from beets.ui import Subcommand
+import urllib
+import urllib2
+
+def sendAlbums(username, password, albumIds):
+    url = 'http://musicbrainz.org/ws/1/collection/' #Where to send them
+    data = urllib.urlencode({ 'add': ",".join(albumIds) })
+    #Post it
+    pm = urllib2.HTTPPasswordMgrWithDefaultRealm()
+    pm.add_password(None, url, username, password)
+    urllib2.install_opener(urllib2.build_opener(
+            urllib2.HTTPDigestAuthHandler(pm)))
+    resp = urllib2.urlopen(url, data).read()
+    return resp
 
 update_mb_collection = Subcommand('mbupdate',
         help='Update MusicBrainz collection')
